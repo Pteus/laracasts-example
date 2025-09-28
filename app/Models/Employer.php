875d425563d927e1bedade2f1eaba8441973d453
Pedro Mateus <pedropmateus91@gmail.com>
2025-09-29 00:28:38 +0100

    Authorization

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,9 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * @mixin IdeHelperEmployer
+ * @property int $id
+ * @property int $user_id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Job> $jobs
+ * @property-read int|null $jobs_count
+ * @property-read \App\Models\User|null $user
+ * @method static \Database\Factories\EmployerFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Employer whereUserId($value)
+ * @mixin \Eloquent
  */
 class Employer extends Model
 {
@@ -16,5 +35,10 @@ class Employer extends Model
     public function jobs()
     {
         return $this->hasMany(Job::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,6 +9,26 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 /**
  * @mixin IdeHelperJob
+ * @property int $id
+ * @property int $employer_id
+ * @property string $title
+ * @property string $salary
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Employer|null $employer
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Tag> $tags
+ * @property-read int|null $tags_count
+ * @method static \Database\Factories\JobFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Job newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Job newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Job query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Job whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Job whereEmployerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Job whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Job whereSalary($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Job whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Job whereUpdatedAt($value)
+ * @mixin \Eloquent
  */
 class Job extends Model
 {
